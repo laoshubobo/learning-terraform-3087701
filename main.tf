a322ea8d287c662289dd9cb109604b49b2f2fc82
Bo <42447234+laoshubobo@users.noreply.github.com>
2024-01-23 17:14:09 -0800

    10

--- main.tf
+++ main.tf
@@ -67,12 +67,13 @@ module "blog_alb" {
     }
   ]
 
-  listeners = {
-    ex-http-https-redirect = {
+  http_tcp_listeners = [
+    {
       port     = 80
       protocol = "HTTP"
+      target_group_index = 0
     }
-  }
+  ]
 
   tags = {
     Environment = "dev"
