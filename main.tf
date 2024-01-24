26391bfabae3c09cd5016efb4ef5b54bb98d1aac
Bo <42447234+laoshubobo@users.noreply.github.com>
2024-01-23 16:00:48 -0800

    Commented

--- main.tf
+++ main.tf
@@ -42,6 +42,7 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
+/*
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
@@ -77,3 +78,4 @@ resource "aws_security_group_rule" "blog_everything_out" {
 
   security_group_id = aws_security_group.blog.id
 }
+*/
