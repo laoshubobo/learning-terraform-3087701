51c910bc32c88c07b1ce183a6e49db9101f7acb5
Bo <42447234+laoshubobo@users.noreply.github.com>
2024-01-23 17:11:05 -0800

    8

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ module "autoscaling" {
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_groups_arns = module.blog_alb.target_groups_arns
-  security_groups= = [module.blog_sg.security_group_id]
+  security_groups = [module.blog_sg.security_group_id]
 
   image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -51,7 +51,7 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~>6.0"
   name    = "blog-alb"
-  load_balancer_type
+  load_balancer_type = "application"
 
   vpc_id  = "module.blog_vpc.vpc_id"
   subnets = module.blog_vpc.public_subnets
