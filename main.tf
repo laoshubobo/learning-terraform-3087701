25bbaabab1d40ab24ba5531e1551fb0671f44378
Bo <42447234+laoshubobo@users.noreply.github.com>
2024-01-23 17:08:26 -0800

    7

--- main.tf
+++ main.tf
@@ -14,9 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-#data "aws_vpc" "default" {
-# default = true
-#}
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
@@ -34,22 +31,28 @@ module "blog_vpc" {
 }
 
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.3.1"
+  # insert the 1 required variable here
+  name = "blog"
+  min_size = 1
+  max_size = 2
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_groups_arns = module.blog_alb.target_groups_arns
+  security_groups= = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
-  subnet_id = module.blog_vpc.public_subnets[0]
-
-  tags = {
-    Name = "Learning Terraform"
-  }
 }
 
-
-module "alb" {
+module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
-
+  version = "~>6.0"
   name    = "blog-alb"
+  load_balancer_type
+
   vpc_id  = "module.blog_vpc.vpc_id"
   subnets = module.blog_vpc.public_subnets
 
